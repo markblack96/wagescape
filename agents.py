983bb572ac0bf31c2942cad4e2f0977d1c8770a9
eblack <eblack@citytelecoin.com>
2019-07-26 15:30:17 -0500

    Basic model, agent implementation

--- agents.py
+++ agents.py
@@ -1,1 +1,14 @@
 from mesa import Agent
+
+"""
+
+"""
+
+class Wagent(Agent):
+    """Agent for wage simulation, represents a single economic actor"""
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.bag = [] # start with nothing
+
+    def step(self):
+        pass
--- model.py
+++ model.py
@@ -1,1 +1,19 @@
 from mesa import Model
+from mesa.time import RandomActivation
+from agents import Wagent
+
+class WageModel(Model):
+    """A model with some number of agents."""
+    def __init__(self, N):
+        self.num_agents = N
+        self.schedule = RandomActivation(self)
+        # create agents
+        for i in range(self.num_agents):
+            a = Wagent(i, self)
+            self.schedule.add(a)
+
+    def step(self):
+        """Advances models by one step."""
+        self.schedule.step()
+
+
